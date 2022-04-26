30af9dcc3bfc5578bda2a4aef3f2f80a951d09b3
SuperPro228 <104450154+SuperPro228@users.noreply.github.com>
2022-04-26 16:32:27 +0700

    Calculator
    calculator

---
+++ sa.py
@@ -0,0 +1,17 @@
+import os
+import time
+import math
+
+what = input("Enter sing/n(+-):")
+a = input("1 number:")
+b = input("2 number:")
+if what == "+":
+    c = int(a)+int(b)
+    print(c)
+if what == "-":
+    c = int(a)-int(b)
+    print(c)
+print("thx for using♡")
+
+time.sleep(1)
+
